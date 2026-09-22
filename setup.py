99a9b0235d88dbb18987de06c90888858d6bf94f
Miller Engelbrecht <millerengelbrecht@Millers-MacBook-Pro.local>
2026-09-22 23:40:13 +1000

    Add pip config for private index

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 """Setup for report-theme-sync."""
 
-import pathlib
 from setuptools import setup
-
-_here = pathlib.Path(__file__).parent
-_readme = (_readme_path / "README.md").read_text() if (_readme_path := _here / "README.md").exists() else ""
 
 setup(
     name="report-theme-sync",
